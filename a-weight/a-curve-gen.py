8df9138c8e9166d7f920a7c634a1f5121eaf5283
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-02-14 15:04:56 +0100

    convert results arr to text, remove []

--- a-weight/a-curve-gen.py
+++ a-weight/a-curve-gen.py
@@ -7,8 +7,9 @@ start = 43
 end = coefficients * step
 results_array = []
 
-print 'calculating a-weight curve size', coefficients, ' coefficients with ', step, 'Hz resolution...'
-print 'start: ', start, ' end: ', end, 'step: ', step
+print '// this file is generatet by phyton script a-curve-gen.py'
+print '// calculating a-weight curve size', coefficients, ' coefficients with ', step, 'Hz resolution'
+print '// start: ', start, ' end: ', end, 'step: ', step
 print ''
 
 i = 0
@@ -18,5 +19,12 @@ for f in range(start, end+1, step):
     results_array.append(coeff)
     i = i + 1
 
+# convert array to string
+results_string = ', '.join([str(elem) for elem in results_array])
 
-print results_array
+# remove firs and last character "[" and "]"
+# results_array = results_array[1:-1]
+
+print 'float a_weighting_curve[512] = {'
+print results_string
+print '};'
